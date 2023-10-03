1b880b0afa92664f87e9c8a8214d560ee74a65af
valerol <valerol@ya.ru>
2023-10-03 18:10:06 +1000

    lesson 2 edited

--- lesson2/task2.java
+++ lesson2/task2.java
@@ -1,16 +1,16 @@
 public class task2 {
     public static void main(String[] args) throws Exception {
-        int[] intArray = {1,2,3};
-        try {
-            int d = 0;
-            double catchedRes1 = intArray[8] / d;
-            System.out.println("catchedRes1 = " + catchedRes1);
-        } catch (ArithmeticException e) {
-            System.out.println("Catching exception: " + e);
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } finally {
-            System.out.println("Bye!");
+        int d = 0;
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        if (intArray.length > 8) {
+            if (d != 0) {
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } else {
+                throw new ArithmeticException("Деление на 0");                    
+            }
+        } else {
+            throw new IndexOutOfBoundsException("Индекс за пределами массива");
         }
     }
 }
--- lesson2/task3.java
+++ lesson2/task3.java
@@ -2,25 +2,23 @@ import java.io.FileNotFoundException;
 
 public class task3 {
     public static void main(String[] args) throws Exception {
-        try {
-            int a = 90;
-            int b = 3;
-            System.out.println(a / b);
-            printSum(23, 234);
-            int[] abc = { 1, 2 };
-            abc[3] = 9;
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
-        } finally {
-            System.out.println("Bye!");
+        int a = 90;
+        int b = 3;
+        System.out.println(a / b);
+        printSum(23, 234);
+        int[] abc = null;
+        if (abc != null) {
+            if (abc.length > 3) {
+                abc[3] = 9;
+            } else {
+                throw new IndexOutOfBoundsException("Массив выходит за пределы своего размера!");
+            }  
+        } else {
+            throw new NullPointerException("Указатель не может указывать на null!");
         }
     }
 
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+    public static void printSum(Integer a, Integer b) {
         System.out.println(a + b);
     }
 
